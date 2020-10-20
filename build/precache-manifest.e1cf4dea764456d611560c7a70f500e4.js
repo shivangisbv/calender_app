04684069d27dc0dc624fc005556d372b77801c02
shivangi <35165444+shivangisbv@users.noreply.github.com>
2020-10-20 16:41:16 +0530

    Add files via upload
    adding the build folder

---
+++ build/precache-manifest.e1cf4dea764456d611560c7a70f500e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40eafed4b2b29ca6b63c99a38f6cdf1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4b99928374adc22169c",
+    "url": "/static/css/2.4d621450.chunk.css"
+  },
+  {
+    "revision": "4f52be99cb4e5c8123c3",
+    "url": "/static/css/main.4ce590ef.chunk.css"
+  },
+  {
+    "revision": "c4b99928374adc22169c",
+    "url": "/static/js/2.da542b47.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.da542b47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f52be99cb4e5c8123c3",
+    "url": "/static/js/main.2aaffd6e.chunk.js"
+  },
+  {
+    "revision": "4d28213e1537c9484596",
+    "url": "/static/js/runtime-main.fe6f8608.js"
+  }
+]);
